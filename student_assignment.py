773d7e47781fb4f0edff207359239540a987152f
Kyle Wang <kyle_wang@asus.com>
2025-02-12 16:16:59 +0800

    hw02_1

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,15 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    loader = PyPDFLoader(q1_pdf)
+    docs = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_overlap=0)
+    chunks = text_splitter.split_documents(docs)
+    return chunks[-1]
 
 def hw02_2(q2_pdf):
     pass
+
+if __name__ == "__main__":
+    print(hw02_1(q1_pdf))
+    hw02_2(q2_pdf)
